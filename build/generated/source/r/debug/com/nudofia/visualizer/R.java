f46ed6e342f51ca31146e1f2685f709383cf8b2c
NoahTU <strickerx5@gmail.com>
2017-03-05 21:37:19 -0500

    v2: New Temp visual settings were added. Next is to apply pony elements within the view and also change certain musice settings.

--- build/generated/source/r/debug/com/nudofia/visualizer/R.java
+++ build/generated/source/r/debug/com/nudofia/visualizer/R.java
@@ -15,14 +15,18 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int but1=0x7f060001;
+        public static final int button2=0x7f060002;
         public static final int visualizerView=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int mainmen=0x7f030001;
     }
     public static final class raw {
-        public static final int test=0x7f040000;
-        public static final int workaround_1min=0x7f040001;
+        public static final int mansion=0x7f040000;
+        public static final int test=0x7f040001;
+        public static final int workaround_1min=0x7f040002;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
